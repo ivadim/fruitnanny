988a8d1b3670116fc477ec3e6cac18d2746f4d4d
woscho <32386911+woscho@users.noreply.github.com>
2017-11-06 20:49:28 +0100

    removed double string in gst-launch config

--- bin/audio.sh
+++ bin/audio.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gst-launch-1.0 -v alsasrc device=device=hw:1 ! audioconvert ! audioresample ! opusenc ! rtpopuspay ! queue max-size-bytes=0 max-size-buffers=0 ! udpsink host=127.0.0.1 port=5002
+gst-launch-1.0 -v alsasrc device=hw:1 ! audioconvert ! audioresample ! opusenc ! rtpopuspay ! queue max-size-bytes=0 max-size-buffers=0 ! udpsink host=127.0.0.1 port=5002
